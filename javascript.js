822e86f1f031ac1f56687c71b94b2c4a7d097f75
mertavcioglu <mavciog1@binghamton.edu>
2022-11-17 15:50:34 +0300

    Added JS exp. and made a playable game.

--- javascript.js
+++ javascript.js
@@ -1,42 +1,49 @@
-function getComputerChoice() {
-  let computerChoice = (Math.floor(Math.random() * 3 + 1));
-  return computerChoice;
-}
-
-let playerChoice = prompt("Enter your choice: 1 for Rock, 2 for Paper, 3 Scissors")
+let playerChoice;
+let computerChoice;
+let thisRoundResult;
 let playerWinCounter = 0;
 let computerWinCounter = 0;
 let drawCounter = 0;
-let i = 1;
-let roundCount = 5;
 
-function playGame() {
-  while (i <= roundCount) {
-    console.log("ROUND", i)
-    playRound(playerChoice, getComputerChoice())
-    console.log("*******************")
-    i++;
-  }
-}
-
-function playRound(playerChoice, computerChoice) {
+function playRound() {
+  computerChoice = (Math.floor(Math.random() * 3 + 1));
   if (computerChoice == playerChoice) {
-    console.log("Draw!");
+    thisRoundResult = "Draw!"
     drawCounter++;
   }
-  else if ((computerChoice == 3 && playerChoice == 2) || (computerChoice == 2 && playerChoice == 1) || (computerChoice == 1 && playerChoice == 3)) {
-    console.log("Computer Wins! Rock beat Scissors");
+  else if ((playerChoice == 2 && computerChoice == 3) || (playerChoice == 1 && computerChoice == 2) || (playerChoice == 3 && computerChoice == 1)) {
+    thisRoundResult = "Computer Wins! Rock beat Scissors"
     computerWinCounter++;
   } else {
-    console.log("Player Wins! Rock beat Scissors");
+    thisRoundResult = "Player Wins! Rock beat Scissors"
     playerWinCounter++;
+  }
+  document.getElementById("playerWinCounter").innerHTML = playerWinCounter;
+  document.getElementById("computerWinCounter").innerHTML = computerWinCounter;
+  document.getElementById("drawCounter").innerHTML = drawCounter;
+  document.getElementById("thisRoundResult").innerHTML = thisRoundResult;
+
+  if (playerWinCounter == 5) {
+    document.getElementById("announceWinner").innerHTML = "PLAYER WON THE GAME";
+  } else if (computerWinCounter == 5) {
+    document.getElementById("announceWinner").innerHTML = "COMPUTER WON THE GAME";
   }
 }
 
-playGame(roundCount)
+const e1 = document.getElementById("rockButton");
+e1.addEventListener("click", function () {
+  playerChoice = 1;
+  playRound();
+});
 
-console.log("***** RESULTS *****")
-console.log("Player win counter:", playerWinCounter)
-console.log("Computer win counter:", computerWinCounter)
-console.log("Draw counter:", drawCounter)
+const e2 = document.getElementById("paperButton");
+e2.addEventListener("click", function () {
+  playerChoice = 2;
+  playRound();
+});
 
+const e3 = document.getElementById("scissorsButton");
+e3.addEventListener("click", function () {
+  playerChoice = 3;
+  playRound();
+});
